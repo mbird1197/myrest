66bf30b678d1ca3c0f3325f9d08ce83f9bae0496
Mark Bird <m_bird2@mail.fhsu.edu>
2024-03-21 07:26:43 -0700

    Updated

--- api/authors/read.php
+++ api/authors/read.php
@@ -56,8 +56,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     }
 
     // Turn to JSON & output
-    echo json_encode($posts_arr);
-
+   // echo json_encode($posts_arr);
+   print_r(json_encode($post_arr));
   } else {
     // No Posts
     echo json_encode(
